7033e327c457140bb80e8913fa626af66fc5d6ed
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2024-06-05 22:48:25 +0700

    Update main3.py

--- main3.py
+++ main3.py
@@ -39,7 +39,7 @@ usernames6 = ["tvc.gym","ngthanhlong021006","kenvo12022001","minhnam2004","minh.
 usernames7 = ["hy.calis","bimostreetworkout.id","kenvo12022001","vanbinh0906","duc_trong9x"]
 usernames8 = ["hoanganh1112022","tuntl675", "yeuem2003n","thanabodxx","minh.ha0102","quang_fitness"]
 usernames9 = ["chulongcuaemm","141201nth", "voquyhuyyy24102001","manhhiucute","hieuvilai2007","trun_trun.32"]
-usernames10 = ["anhhhh_tu","tuanhamez","sinhan68","jdnummer1"]
+usernames10 = ["anhhhh_tu","tuanhamez","sinhan68","jdnummer1","minhnguyen_ifbb","duyanh_15112003"]
 # Mapping choices to actual lists
 username_lists = {
     'usernames1': usernames1,
