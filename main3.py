29cdf3960c6e419d014d0ca2035b2e78c598a3ff
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2024-06-11 00:16:36 +0700

    Update main3.py

--- main3.py
+++ main3.py
@@ -31,15 +31,16 @@ def run_command(username, output):
 
 # Danh sách các username
 usernames1 = ["park.77m1","bonbarber2210","minhly06_01", "hunggymcalis", "quocvu93","nienty28", "buiquoc_sw","truongminhdung1904","anhhhh_tu","hongduong.1199"]
-usernames2 = ["honguynvn04","365.ngy.nh.em","herst277","trinhatquankhu1","thang_masic","gymerteamhonda"]
-usernames3 = ["duongkimochii","duongtapxo","congmanhfitness","_nmd04","hieuluoitap","fu_nguyen68","kiennguyenvan0302"]
+usernames2 = ["honguynvn04","365.ngy.nh.em","herst277","trinhatquankhu1","thang_masic","gymerteamhonda","kimnguyennanh"]
+usernames3 = ["duongkimochii","duongtapxo","congmanhfitness","_nmd04","hieuluoitap","fu_nguyen68","phongbum2002"]
 usernames4 = ["hmatuan9","huutinh103","anhbodoi6mui","ikram_rmdn","kieubanana","cokhiminhtrung","hungrambo1_"]
-usernames5 = ["hpp358965","rforrachman","ntf_205","rhenz_win","ashuracalisthenicsteam","nihao22012003","nien_2006"]
-usernames6 = ["tvc.gym","ngthanhlong021006","kenvo12022001","minhnam2004","minh.ha0102","tienfit","bachmahoangtu2k"]
-usernames7 = ["hy.calis","bimostreetworkout.id","kenvo12022001","vanbinh0906","duc_trong9x","huyvo.1510","nguyentruongson2294"]
+usernames5 = ["hpp358965","rforrachman","ntf_205","rhenz_win","ashuracalisthenicsteam","nihao22012003","dp_120796"]
+usernames6 = ["tvc.gym","ngthanhlong021006","minhnam2004","minh.ha0102","tienfit","anhtung_0510","vanhcalis2007"]
+usernames7 = ["hy.calis","bimostreetworkout.id","kenvo12022001","vanbinh0906","duc_trong9x","huyvo.1510","hung01082004"]
 usernames8 = ["hoanganh1112022","tuntl675", "yeuem2003n","thanabodxx","minh.ha0102","quang_fitness","huuhieulam","nxp.fitness","thanhthong2608"]
 usernames9 = ["chulongcuaemm","141201nth", "voquyhuyyy24102001","manhhiucute","hieuvilai2007","trun_trun.32"]
-usernames10 = ["anhhhh_tu","tuanhamez","sinhan68","jdnummer1","minhnguyen_ifbb","duyanh_15112003","bee.calisthenics"]
+usernames10 = ["anhhhh_tu","tuanhamez","sinhan68","jdnummer1","minhnguyen_ifbb","duyanh_15112003","nxp.fitness","thanhthong2608","bee.calisthenics"]
+
 # Mapping choices to actual lists
 username_lists = {
     'usernames1': usernames1,
