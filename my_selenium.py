a119716915084fcac15389e86e593f9d2feaf97e
Chip Huyen <thanh6301@gmail.com>
2024-08-01 23:55:19 +0700

    Update my_selenium.py

--- my_selenium.py
+++ my_selenium.py
@@ -4,29 +4,28 @@ from selenium.webdriver.common.by import By
 import time
 import threading
 import multiprocessing
-import psutil
 
 def run_thread(keyword):
     driver = webdriver.Chrome()
 
     try: 
         driver.get("https://www.youtube.com/")
+
+        # Tìm ô tìm kiếm
         search_box = driver.find_element(By.XPATH, '//input[@id="search"]')
+
+        # Gõ từ khóa vào ô tìm kiếm
         search_box.send_keys("@Boymuscleworkout")
+
+        # Nhấn Enter để tìm kiếm
         search_box.send_keys(Keys.RETURN)
         time.sleep(5)
 
+        # Tìm và nhấp vào liên kết của kênh
         video_link = driver.find_element(By.XPATH, '//*[@id="subscribers" and contains(text(), "@Boymuscleworkout")]')
         video_link.click()
         time.sleep(5)
-
-        playlists = driver.find_element(By.XPATH, '//yt-tab-shape[@tab-title="Playlists"]')
-        playlists.click()
-        time.sleep(5)
-        watch_video = driver.find_element(By.XPATH, '//a[@id="video-title" and contains(text(), "{}")]'.format(keyword))
-        watch_video.click()
-        time.sleep(10)
-        
+        # Define the XPath of the button
         xpath = '//button[@aria-label="Loop playlist"]'
 
         # Find the button element by XPath
@@ -34,22 +33,27 @@ def run_thread(keyword):
 
         # Click the button using JavaScript
         driver.execute_script("arguments[0].click();", element)
-
+    
 
         while True:
+            # Chụp ảnh màn hình
             driver.save_screenshot("screenshot_{}_{}.png".format(keyword, time.time()))
             print("Screenshot taken for keyword: {}".format(keyword))
+
+            # Chờ 10 phút trước khi chụp ảnh màn hình tiếp theo
             time.sleep(300)
 
+        #Đóng trình duyệt khi kết thúc
         driver.quit()
-    
-    except:
+
+    except: 
+
         driver.get("https://www.youtube.com/@Boymuscleworkout/playlists")
         time.sleep(10)
         watch_video = driver.find_element(By.XPATH, '//a[@id="video-title" and contains(text(), "{}")]'.format(keyword))
         watch_video.click()
         time.sleep(10)
-        
+        # Define the XPath of the button
         xpath = '//button[@aria-label="Loop playlist"]'
 
         # Find the button element by XPath
@@ -58,53 +62,46 @@ def run_thread(keyword):
         # Click the button using JavaScript
         driver.execute_script("arguments[0].click();", element)
 
-
         while True:
+            # Chụp ảnh màn hình
             driver.save_screenshot("screenshot_{}_{}.png".format(keyword, time.time()))
             print("Screenshot taken for keyword: {}".format(keyword))
+
+            # Chờ 10 phút trước khi chụp ảnh màn hình tiếp theo
             time.sleep(300)
 
+        # Đóng trình duyệt khi kết thúc
         driver.quit()
 
-def resource_check():
-    cpu_usage = psutil.cpu_percent(interval=1)
-    ram_usage = psutil.virtual_memory().percent
-    return cpu_usage < 90 and ram_usage < 90
 
 def run_process(elements):
+    # Tạo và khởi chạy các luồng trong mỗi process
     threads = []
-    element_index = 0
+    for element in elements:
+        thread = threading.Thread(target=run_thread, args=(element,))
+        thread.start()
+        threads.append(thread)
 
-    while True:
-        if element_index >= len(elements):
-            break
-        
-        if resource_check():
-            element = elements[element_index]
-            thread = threading.Thread(target=run_thread, args=(element,))
-            thread.start()
-            threads.append(thread)
-            element_index += 1
-        else:
-            print("High resource usage detected. Waiting...")
-            time.sleep(60)  # Chờ một chút trước khi kiểm tra lại
-
-    # Chờ tất cả các thread hoàn thành
+    # Chờ tất cả các luồng hoàn thành trong mỗi process
     for thread in threads:
         thread.join()
 
-if __name__ == "__main__":
-    elements = ["honguynvn04", "hieuvilai2007"]
-    elements = elements * 600  # Lặp lại các phần tử để tạo danh sách dài hơn
 
+if __name__ == "__main__":
+    # List các elements bạn muốn xử lý
+    elements = ["honguynvn04","hieuvilai2007"]
+    elements = elements * 25
+
+    # Số lượng process muốn tạo, có thể sử dụng multiprocessing.cpu_count() để lấy số lượng core CPU
     num_processes = multiprocessing.cpu_count() - 1
+
+    # Tạo và khởi chạy các process
     processes = []
     for i in range(num_processes):
         process = multiprocessing.Process(target=run_process, args=(elements,))
         process.start()
         processes.append(process)
 
+    # Chờ tất cả các process hoàn thành
     for process in processes:
         process.join()
-
-    print("Done!")
